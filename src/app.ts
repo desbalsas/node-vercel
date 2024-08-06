0fe3732dc05fb78253750a052e5d81c550fc6bf0
Vicente Santamaria Velasco <desarrollo@balsas.com.mx>
2024-08-06 12:33:07 -0600

    remove morgan

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,29 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
 
-import * as middlewares from './middlewares';
-import api from './api';
-import MessageResponse from './interfaces/MessageResponse';
+import * as middlewares from './middlewares'
+import api from './api'
+import type MessageResponse from './interfaces/MessageResponse'
 
-require('dotenv').config();
+require('dotenv').config()
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+//app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
-});
+app.get<{}, MessageResponse>('/', (_, res) => {
+	res.json({
+		message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+	})
+})
 
-app.use('/api/v1', api);
+app.use('/api/v1', api)
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
 
-export default app;
+export default app
