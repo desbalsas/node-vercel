968e8d65aa657271799c08a63ccc56c242f37ad3
Vicente Santamaria Velasco <desarrollo@balsas.com.mx>
2024-08-06 12:38:23 -0600

    add favicon

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,8 @@ const app = express()
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
+// Middleware para servir archivos estáticos
+app.use(express.static('public'))
 
 app.get<{}, MessageResponse>('/', (_, res) => {
 	res.json({
